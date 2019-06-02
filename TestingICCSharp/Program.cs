71d98dfae0cb5c856e0210d6b314a54395de58e3
redradist <redradist@gmail.com>
2019-06-02 16:23:42 +0300

    Removed Test project

--- TestingICCSharp/Program.cs
+++ TestingICCSharp/Program.cs
@@ -16,30 +16,5 @@ namespace TestingICCSharp
             await Task.Delay(1000);
             Console.WriteLine($"CurrentThreadId is {Thread.CurrentThread.ManagedThreadId}");
         }
-        
-        static void Main(string[] args)
-        {
-            TcpServer server = new TcpServer(typeof(TcpClient));
-            server.StartServer("127.0.0.1", "49000");
-//            Component parent = new Component();
-//            Console.WriteLine("Start task");
-//            Task task = parent.StartTask(function: async () =>
-//            {
-//                Stopwatch sw = new Stopwatch();
-//                sw.Start();
-//                await DoSomething();
-//                sw.Stop();
-//                Console.WriteLine($"Elapsed={sw.Elapsed}");
-//                parent.Stop();
-//            });
-//            parent.Run();
-//            var thread = new Thread(() =>
-//            {
-//                parent.Run();
-//            });
-//            thread.Start();
-//            thread.Join();
-            Console.WriteLine("Finish awaiting task !!");
-        }
     }
 }
